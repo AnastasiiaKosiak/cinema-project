8ef3937bd0bdef24a1d7699cf8b952b9a6690de1
Anastasiia Kosiak <cctvporsh@gmail.com>
2020-05-28 13:48:43 +0300

    Added Order model and its DAO class (#5)
    * Added Order model and its DAO class
    
    * implemented clear() method
    
    * Used @Inject annotation to get ShoppingCartService
    
    * removed unused imports

---
+++ src/main/java/mate/academy/cinema/service/impl/OrderServiceImpl.java
@@ -0,0 +1,42 @@
+package mate.academy.cinema.service.impl;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import mate.academy.cinema.dao.OrderDao;
+import mate.academy.cinema.dao.ShoppingCartDao;
+import mate.academy.cinema.lib.Inject;
+import mate.academy.cinema.lib.Service;
+import mate.academy.cinema.model.Order;
+import mate.academy.cinema.model.ShoppingCart;
+import mate.academy.cinema.model.Ticket;
+import mate.academy.cinema.model.User;
+import mate.academy.cinema.service.OrderService;
+import mate.academy.cinema.service.ShoppingCartService;
+
+@Service
+public class OrderServiceImpl implements OrderService {
+    @Inject
+    private OrderDao orderDao;
+    @Inject
+    private ShoppingCartDao shoppingCartDao;
+    @Inject
+    private ShoppingCartService shoppingCartService;
+
+    @Override
+    public Order completeOrder(List<Ticket> tickets, User user) {
+        List<Ticket> newTicketsList = List.copyOf(tickets);
+        Order newOrder = new Order();
+        newOrder.setOrderDate(LocalDateTime.now());
+        newOrder.setUser(user);
+        newOrder.setTickets(newTicketsList);
+        ShoppingCart shoppingCart = shoppingCartDao.getByUser(user);
+        shoppingCartService.clear(shoppingCart);
+        return orderDao.add(newOrder);
+
+    }
+
+    @Override
+    public List<Order> getOrderHistory(User user) {
+        return orderDao.getOrderHistory(user);
+    }
+}
